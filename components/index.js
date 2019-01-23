674e228f6f901b8b6f575050544e680a5ccb3023
Jim Jin <jim_king_2000@163.com>
2019-01-23 21:25:06 +0800

    refactory

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import GlobalNavigation from '@atlaskit/global-navigation';
 import { MdDirectionsBus } from "react-icons/md";
 
-export default ({ isAuthenticated }) => (
+export default () => (
   <GlobalNavigation
     productIcon={MdDirectionsBus}
     productHref='/'
