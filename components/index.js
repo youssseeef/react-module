c140c52f677ad648b923668c7bf0c806648b66b4
Jim Jin <jim_king_2000@163.com>
2019-01-23 22:43:23 +0800

    refactory

--- components/index.js
+++ components/index.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import GlobalNavigation from '@atlaskit/global-navigation';
 import { MdDirectionsBus } from "react-icons/md";
-import { DropdownItemGroup, DropdownItem } from '@atlaskit/dropdown-menu';
-import Router from 'next/router';
 
-const LoginDropdown = () => (
-  <DropdownItemGroup>
-    <DropdownItem href='/login'>
-      Login
-    </DropdownItem>
-  </DropdownItemGroup>
-);
-
-const LogoutDropdown = () => (
-  <DropdownItemGroup>
-    <DropdownItem href='/logout'>
-      Logout
-    </DropdownItem>
-  </DropdownItemGroup>
-);
-
-function onProductClick() {
-  Router.push('/');
-}
-
-export const NavBar = ({ isAuthenticated }) => (
+export const NavBar = () => (
   <GlobalNavigation
     productIcon={MdDirectionsBus}
-    productTooltip='Scenario Bus'
-    onProductClick={onProductClick}
-    profileItems={isAuthenticated ? LogoutDropdown : LoginDropdown}
+    productHref='/'
   />
 );
