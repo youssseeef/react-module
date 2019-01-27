ef4ec8f533252a2beb6136ee63610b20e94d65a1
Youssef Salama <youssefhanii@hotmail.com>
2019-01-27 02:19:02 +0200

    added client check

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import Router from 'next/router';
 
-export const B = () => <button onClick={() => Router.push('/')}>click</button>;
+export const B = () => {
+    const isClient = typeof document !== 'undefined';
+    return (<button onClick={() => isClient && Router.push('/')}>click</button>);
+}
