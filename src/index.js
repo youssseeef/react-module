2464ad488c541e0d209cd8f9a17ba6ead204bb03
Youssef Salama <youssefhanii@hotmail.com>
2019-01-27 02:26:00 +0200

    another client test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import Router from 'next/router';
 
 export const B = () => {
     console.log('test');
-    const isClient = typeof document !== 'undefined';
+    const isClient = process.browser;
     return (<button onClick={() => isClient && Router.push('/')}>click</button>);
 }
