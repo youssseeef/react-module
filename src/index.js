2b65aa191e604bc25ef2b2404ecb746910dca546
Youssef Salama <youssefhanii@hotmail.com>
2019-01-27 03:12:59 +0200

    default export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import Router from 'next/router';
 
-export default B = () => <button onClick={() => Router.push('/')}>click</button>;
+export default () => <button onClick={() => Router.push('/')}>click</button>;
