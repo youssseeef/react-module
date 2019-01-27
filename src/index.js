6490b8671a78ec94bd1cda35e1cab62cd5b830cd
Youssef Salama <youssefhanii@hotmail.com>
2019-01-27 02:23:12 +0200

    modified with log and client test and compiled

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from 'next/router';
 
 export const B = () => {
+    console.log('test');
     const isClient = typeof document !== 'undefined';
     return (<button onClick={() => isClient && Router.push('/')}>click</button>);
 }
