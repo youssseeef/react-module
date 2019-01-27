13f31db0830565facd5300989e232c70ddbc6238
Youssef Salama <youssefhanii@hotmail.com>
2019-01-27 03:41:14 +0200

    vwerwver

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import dynamic from 'next/dynamic'
 const Router = dynamic(() => import('next/router'), {
     ssr: false
 });
-
+console.log(Router)
 export default () => <button onClick={() => process.browser && Router.push('/')}>click</button>;
